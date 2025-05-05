9de011573a270ddcb3b029ac328c4402651c598b
sofia <ssarmiento088@gmail.com>
2025-05-04 23:29:53 -0500

    updating apps.database

--- apps/database/apps.py
+++ apps/database/apps.py
@@ -8,11 +8,14 @@ class DatabaseConfig(AppConfig):
     name = 'apps.database'
 
     def ready(self):
-        mongoengine.connect(
-            db=os.getenv('MONGO_DB_NAME'),
-            host=os.getenv('MONGO_HOST'),
-            port=int(os.getenv('MONGO_PORT')),
-            username=os.getenv('MONGO_USERNAME'),
-            password=os.getenv('MONGO_PASSWORD'),
-            authentication_source=os.getenv('MONGO_AUTH_SOURCE')
-        )
+        try:
+            mongoengine.connect(
+                db=os.getenv('MONGO_DB_NAME'),
+                host=os.getenv('MONGO_HOST'),
+                port=int(os.getenv('MONGO_PORT')),
+                username=os.getenv('MONGO_USERNAME'),
+                password=os.getenv('MONGO_PASSWORD'),
+                authentication_source=os.getenv('MONGO_AUTH_SOURCE')
+            )
+        except Exception as e:
+            print(f"Error connecting to MongoDB: {e}")
