73df5c189ad6563ee15f072fad311425e46c6c7c
FranzARCH <franzchipana2610@gmail.com>
2025-08-05 16:44:28 -0500

    create urls

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('classrooms/', views.classroomsViews)
+    path('deliveries/', views.deliveriesViews)
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def deliveriesView(request):
+    deliveries = [
+        {'id': 1, 'fecha': '2023-01-01'},
+        {'id': 2, 'fecha': '2023-01-02'},
+        {'id': 3, 'fecha': '2023-01-03'},
+    ] 
+    return JsonResponse(deliveries)
