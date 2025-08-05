93b8ae3d49048eb078536f1e2ae5dc411f3f2e22
FranzARCH <franzchipana2610@gmail.com>
2025-08-05 16:32:40 -0500

    Create urls

--- apps/deliveries/views.py
+++ apps/deliveries/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def deliveries(request):
+
+    deliveries = [
+        {'id': 1, 'fecha': '2023-01-01'},
+        {'id': 2, 'fecha': '2023-01-02'},
+        {'id': 3, 'fecha': '2023-01-03'},
+    ]
+
+    return HttpResponse(deliveries)
