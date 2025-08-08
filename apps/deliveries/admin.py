5370999a43f45a46847c5a1a0cdba380aa1a39bf
FranzARCH <franzchipana2610@gmail.com>
2025-08-07 23:06:41 -0500

    Update admin site

--- apps/deliveries/admin.py
+++ apps/deliveries/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Delivery
+from .models import Delivery, UtilDelivery, MoneyDelivery
 
 admin.site.register(Delivery)
+admin.site.register(UtilDelivery)
+admin.site.register(MoneyDelivery)
 
